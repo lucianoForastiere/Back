85e9a9a8dc1f115d58c3c92cec4baa85a0ed9f22
fmarcos38 <fmarcos_23@hotmail.com>
2024-11-27 11:45:58 -0300

    pp

--- src/controllers/propiedad.js
+++ src/controllers/propiedad.js
@@ -1,42 +1,50 @@
 const Propiedad = require("../models/propiedad");
 
-const getPropiedades = async (req, res) => {
-    const { limit, offset, operacion, tipo, precioMin, precioMax } = req.query;
+
+const getPropiedades = async(req, res) => {
+    const { limit, offset, operacion, tipo, precioMin, precioMax } = req.query; 
     try {
         let propiedades;
         let filtros = {};
 
-        // Filtros
-        // Por operacion
-        if (operacion) {
-            filtros["operacion.tipoOperacion"] = operacion;
+        //filtros
+        //por operacion
+        if(operacion){
+            filtros["operacion.tipoOperacion"] = operacion; 
         }
-        // Tipo
-        if (tipo) {
+        //tipo
+        if(tipo){
             filtros.tipoPropiedad = tipo;
         }
-        // Precio MIN
-        if (precioMin) {
-            filtros["operacion.precio"] = { ...filtros["operacion.precio"], $gte: Number(precioMin) };
+        //precio MIN
+        if(precioMin){
+            filtros["operacion.precio"] = {...filtros["operacion.precio"], $gte: Number(precioMin)};
         }
-        // Precio MAX
-        if (precioMax) {
-            filtros["operacion.precio"] = { ...filtros["operacion.precio"], $lte: Number(precioMax) };
+        //precio MAX
+        if(precioMax){
+            filtros["operacion.precio"] = {...filtros["operacion.precio"], $lte: Number(precioMax)};
         }
-        // Sin filtros
-        if (!operacion && !tipo && !precioMin && !precioMax) {
+        //sin filtros
+        if(!operacion && !tipo && !precioMin && !precioMax){
             filtros = {};
         }
 
         propiedades = await Propiedad.find(filtros)
-            .skip(Number(offset) || 0)
-            .limit(Number(limit) || 12)
-            .exec();
+        .skip(Number(offset) || 0)
+        .limit(Number(limit) || 12)
+        .exec();
 
-        res.json(propiedades);
+        //obtengo el total de props q cumplen con los filtros (sin paginación)
+        const totPropsFiltradas = await Propiedad.countDocuments(filtros);
+
+        //envio las 12 props mas el total de las q cumplen los filtros
+        res.status(200).json({
+            totPropsFiltradas,
+            propiedades
+        });
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: error.message });
+        console.log(error);
+        res.status(500).json({ mensaje: "Error del servidor" });
     }
 };
 
@@ -53,8 +61,8 @@ const getPropiedad = async(req, res) => {
         res.status(200).json(propiedad);
     }
     catch (error) {
-        console.error("Error en el controlador getPropiedad:", error);
-        res.status(500).json({ mensaje: "Error del servidor", error: error.message });
+        console.log(error);
+        res.status(500).json({ mensaje: "Error del servidor" });
     }
 };
 
