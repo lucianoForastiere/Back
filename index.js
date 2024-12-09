610c4aa1b2af880b9d491c5ecdf61217753a374f
fmarcos38 <fmarcos_23@hotmail.com>
2024-12-09 15:49:38 -0300

    auth

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ dotenv.config();
 
 //importo rutas
 const routerPropiedad = require('./src/routes/propiedad');
+const routerUsuario = require('./src/routes/usuario');
+const routerAuth = require('./src/routes/auth');
 
 const app = express();
 
@@ -19,6 +21,8 @@ dbConnection();
 
 //declaro rutas
 app.use('/propiedades', routerPropiedad);
+app.use('/usuarios', routerUsuario);
+app.use('/auth', routerAuth);
 
 //puerto
 const PORT = process.env.PORT || 3002;
